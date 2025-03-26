0c0eabcc89f234193b5f0215ee26ce114c3f9112
Ursula Visnapuu <visnapuuursula@gmail.com>
2025-03-26 08:32:51 +0200

    Lisatud Sprint Boot projekti struktuur

---
+++ src/main/java/ee/ursulavisnapuu/lennuplaneerija/LennuplaneerijaApplication.java
@@ -0,0 +1,13 @@
+package ee.ursulavisnapuu.lennuplaneerija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LennuplaneerijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LennuplaneerijaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/ursulavisnapuu/lennuplaneerija/LennuplaneerijaApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.ursulavisnapuu.lennuplaneerija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LennuplaneerijaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
