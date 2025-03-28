33ad23042b83a74b4401144440158504b034694d
Ursula Visnapuu <visnapuuursula@gmail.com>
2025-03-28 11:53:00 +0200

    Lisatud võimalus reisijale istekoht määrata ning testitud edukalt

--- src/main/java/ee/ursulavisnapuu/lennuplaneerija/config/DataLoader.java
+++ src/main/java/ee/ursulavisnapuu/lennuplaneerija/config/DataLoader.java
@@ -1,7 +1,9 @@
 package ee.ursulavisnapuu.lennuplaneerija.config;
 
 import ee.ursulavisnapuu.lennuplaneerija.model.Seat;
+import ee.ursulavisnapuu.lennuplaneerija.model.Passenger;
 import ee.ursulavisnapuu.lennuplaneerija.repository.SeatRepository;
+import ee.ursulavisnapuu.lennuplaneerija.repository.PassengerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +11,11 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
 
     private final SeatRepository seatRepository;
+    private final PassengerRepository passengerRepository;
 
-    public DataLoader(SeatRepository seatRepository) {
+    public DataLoader(SeatRepository seatRepository, PassengerRepository passengerRepository) {
         this.seatRepository = seatRepository;
+        this.passengerRepository = passengerRepository;
     }
 
     @Override
@@ -22,8 +26,14 @@ public class DataLoader implements CommandLineRunner {
             seatRepository.save(Seat.builder().rowNumber(2).seatLetter('C').seatWindow(false).extraLegroom(false).nearExit(false).occupied(false).build());
             seatRepository.save(Seat.builder().rowNumber(3).seatLetter('F').seatWindow(true).extraLegroom(true).nearExit(true).occupied(false).build());
             seatRepository.save(Seat.builder().rowNumber(4).seatLetter('D').seatWindow(false).extraLegroom(true).nearExit(false).occupied(true).build());
+            System.out.println("✔️ Istekohad edukalt lisatud!");
+        }
 
-            System.out.println("\u2714\uFE0F Istekohad edukalt lisatud!");
+        if (passengerRepository.count() == 0) {
+            passengerRepository.save(Passenger.builder().firstName("Mari").lastName("Maasikas").email("mari@example.com").build());
+            passengerRepository.save(Passenger.builder().firstName("Jaan").lastName("Kask").email("jaan@example.com").build());
+            passengerRepository.save(Passenger.builder().firstName("Kati").lastName("Kuusk").email("kati@example.com").build());
+            System.out.println("✔️ Testreisijad edukalt lisatud!");
         }
     }
 }
